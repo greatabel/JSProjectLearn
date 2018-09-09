cd1a834d2fc04e348dd0380a64fa9679ed6c75eb
greatabel <myreceiver2for2github@gmail.com>
2018-09-09 17:38:06 +0800

    global data

--- WXApplet/pages/demo/demo.js
+++ WXApplet/pages/demo/demo.js
@@ -1,8 +1,12 @@
+const app = getApp()
 Page({
   formSubmit: function(e){
+    app.globalData.globalDataTest++
+    console.log(app.globalData.globalDataTest)
     console.log('form发生了submit事件，携带数据为:', e.detail.value)
   },
   formReset: function(){
     console.log('form发生了reset事件')
+    console.log(app.globalData.globalDataTest)
   }
 })
