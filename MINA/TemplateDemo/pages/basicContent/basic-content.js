69b122c4c42ef90a07198e2423054dd432bbd740
greatabel <myreceiver2for2github@gmail.com>
2018-09-13 16:34:55 +0800

    text basic-content

--- MINA/TemplateDemo/pages/basicContent/basic-content.js
+++ MINA/TemplateDemo/pages/basicContent/basic-content.js
@@ -1,3 +1,6 @@
+var initData = 'this is first line\nthis is second line'
+var extraLine = [];
+
 Page({
   data: {
     iconSize: [20, 30, 40, 50, 60, 70],
@@ -6,6 +9,22 @@ Page({
     ],
     iconType: [
       'success', 'success_no_circle', 'info', 'warn', 'waiting', 'cancel', 'download', 'search', 'clear'
-    ]
+    ],
+    text: initData
+  },
+  
+  add: function (e) {
+    extraLine.push('other line')
+    this.setData({
+      text: initData + '\n' + extraLine.join('\n')
+    })
+  },
+  remove: function (e) {
+    if (extraLine.length > 0) {
+      extraLine.pop()
+      this.setData({
+        text: initData + '\n' + extraLine.join('\n')
+      })
+    }
   }
 })
