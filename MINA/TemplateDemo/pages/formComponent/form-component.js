e87c507650e184fec3d9ef4c9906dc656cede46e
greatabel <myreceiver2for2github@gmail.com>
2018-09-14 17:13:08 +0800

    add checkbox

--- MINA/TemplateDemo/pages/formComponent/form-component.js
+++ MINA/TemplateDemo/pages/formComponent/form-component.js
@@ -6,7 +6,14 @@ var pageObject = {
     warnSize: 'default',
     disabled: false,
     plain: false,
-    loading: false
+    loading: false, items: [
+      { name: 'USA', value: '美国' },
+      { name: 'CHN', value: '中国', checked: 'true' },
+      { name: 'BRA', value: '巴西' },
+      { name: 'JPN', value: '日本' },
+      { name: 'ENG', value: '英国' },
+      { name: 'TUR', value: '法国' },
+    ]
   },
   setDisabled: function (e) {
     this.setData({
@@ -28,6 +35,10 @@ var pageObject = {
     console.log(e.detail.userInfo)
     console.log(e.detail.rawData)
   },
+  
+  checkboxChange: function(e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+  }
 }
 
 for (var i = 0; i < types.length; ++i) {
