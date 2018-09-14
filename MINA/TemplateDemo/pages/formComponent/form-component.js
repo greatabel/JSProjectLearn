10ca3e86ef4791c6d66f25b68f8b5f93dd221484
greatabel <myreceiver2for2github@gmail.com>
2018-09-14 17:16:02 +0800

    form

--- MINA/TemplateDemo/pages/formComponent/form-component.js
+++ MINA/TemplateDemo/pages/formComponent/form-component.js
@@ -38,6 +38,12 @@ var pageObject = {
   
   checkboxChange: function(e) {
     console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+  },
+  formSubmit: function (e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+  formReset: function () {
+    console.log('form发生了reset事件')
   }
 }
 
