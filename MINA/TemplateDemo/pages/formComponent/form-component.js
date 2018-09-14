ab534b298907f4adf0855478baa7461a52c44ead
greatabel <myreceiver2for2github@gmail.com>
2018-09-14 17:20:54 +0800

    input

--- MINA/TemplateDemo/pages/formComponent/form-component.js
+++ MINA/TemplateDemo/pages/formComponent/form-component.js
@@ -13,7 +13,9 @@ var pageObject = {
       { name: 'JPN', value: '日本' },
       { name: 'ENG', value: '英国' },
       { name: 'TUR', value: '法国' },
-    ]
+    ],
+    focus: false,
+    inputValue: ''
   },
   setDisabled: function (e) {
     this.setData({
@@ -44,6 +46,35 @@ var pageObject = {
   },
   formReset: function () {
     console.log('form发生了reset事件')
+  },
+  bindButtonTap: function () {
+    this.setData({
+      focus: true
+    })
+  },
+  bindKeyInput: function (e) {
+    this.setData({
+      inputValue: e.detail.value
+    })
+  },
+  bindReplaceInput: function (e) {
+    var value = e.detail.value
+    var pos = e.detail.cursor
+    if (pos != -1) {
+      //光标在中间
+      var left = e.detail.value.slice(0, pos)
+      //计算光标的位置
+      pos = left.replace(/11/g, '2').length
+    }
+
+    //直接返回对象，可以对输入进行过滤处理，同时可以控制光标的位置
+    return {
+      value: value.replace(/11/g, '2'),
+      cursor: pos
+    }
+
+    //或者直接返回字符串,光标在最后边
+    //return value.replace(/11/g,'2'),
   }
 }
 
