c6b88b38754f10efe1a184a6096158f639103690
greatabel <myreceiver2for2github@gmail.com>
2018-09-16 17:15:42 +0800

    date picker

--- MINA/TemplateDemo/pages/formComponent/picker/picker.js
+++ MINA/TemplateDemo/pages/formComponent/picker/picker.js
@@ -2,12 +2,19 @@ Page({
   data: {
     array: ['美国', '中国', '巴西', '日本'],
     index: 0,
+    date: '2018-09-16',
   },
   
   bindPickerChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       index: e.detail.value
+    })
+  },
+  bindDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
     })
   },
 
