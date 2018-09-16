1d3c95c32a714eb88b60c735cb0e2acfc1e8e43f
greatabel <myreceiver2for2github@gmail.com>
2018-09-16 17:18:27 +0800

    region

--- MINA/TemplateDemo/pages/formComponent/picker/picker.js
+++ MINA/TemplateDemo/pages/formComponent/picker/picker.js
@@ -3,6 +3,8 @@ Page({
     array: ['美国', '中国', '巴西', '日本'],
     index: 0,
     date: '2018-09-16',
+    region: ['广东省', '广州市', '海珠区'],
+    customItem: '全部'
   },
   
   bindPickerChange: function (e) {
@@ -17,5 +19,11 @@ Page({
       date: e.detail.value
     })
   },
-
+  bindRegionChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      region: e.detail.value
+    })
+  }
+  
 })
