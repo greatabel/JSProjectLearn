6c2eca02593955426b52a7ec4fd1898c583e2a2f
greatabel <myreceiver2for2github@gmail.com>
2018-09-17 23:06:29 +0800

    switch

--- MINA/TemplateDemo/pages/formComponent/slider/slider.js
+++ MINA/TemplateDemo/pages/formComponent/slider/slider.js
@@ -6,4 +6,10 @@ for (var i = 1; i < 5; i++) {
     }
   })(i)
 }
+pageData['switch1Change'] = function (e){
+    console.log('switch1 发生 change 事件，携带值为', e.detail.value)
+  },
+pageData['switch2Change'] = function (e){
+    console.log('switch2 发生 change 事件，携带值为', e.detail.value)
+  }
 Page(pageData)
