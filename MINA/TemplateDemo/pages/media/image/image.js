e799e84ec80f477a47a15a9e07f5712a4af25001
greatabel <myreceiver2for2github@gmail.com>
2018-09-18 15:19:10 +0800

    image

--- MINA/TemplateDemo/pages/media/image/image.js
+++ MINA/TemplateDemo/pages/media/image/image.js
@@ -1,66 +1,45 @@
-// pages/media/image/image.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    array: [{
+      mode: 'scaleToFill',
+      text: 'scaleToFill：不保持纵横比缩放图片，使图片完全适应'
+    }, {
+      mode: 'aspectFit',
+      text: 'aspectFit：保持纵横比缩放图片，使图片的长边能完全显示出来'
+    }, {
+      mode: 'aspectFill',
+      text: 'aspectFill：保持纵横比缩放图片，只保证图片的短边能完全显示出来'
+    }, {
+      mode: 'top',
+      text: 'top：不缩放图片，只显示图片的顶部区域'
+    }, {
+      mode: 'bottom',
+      text: 'bottom：不缩放图片，只显示图片的底部区域'
+    }, {
+      mode: 'center',
+      text: 'center：不缩放图片，只显示图片的中间区域'
+    }, {
+      mode: 'left',
+      text: 'left：不缩放图片，只显示图片的左边区域'
+    }, {
+      mode: 'right',
+      text: 'right：不缩放图片，只显示图片的右边边区域'
+    }, {
+      mode: 'top left',
+      text: 'top left：不缩放图片，只显示图片的左上边区域'
+    }, {
+      mode: 'top right',
+      text: 'top right：不缩放图片，只显示图片的右上边区域'
+    }, {
+      mode: 'bottom left',
+      text: 'bottom left：不缩放图片，只显示图片的左下边区域'
+    }, {
+      mode: 'bottom right',
+      text: 'bottom right：不缩放图片，只显示图片的右下边区域'
+    }],
+    src: '../../../img/cat.jpg'
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  imageError: function (e) {
+    console.log('image3发生error事件，携带值为', e.detail.errMsg)
   }
 })
