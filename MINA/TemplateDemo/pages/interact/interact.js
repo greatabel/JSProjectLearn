dc05c7ec791715e1070204a215551e22d7d524dc
greatabel <myreceiver2for2github@gmail.com>
2018-09-18 11:14:15 +0800

    modal toast

--- MINA/TemplateDemo/pages/interact/interact.js
+++ MINA/TemplateDemo/pages/interact/interact.js
@@ -1,7 +1,9 @@
 Page({
   data: {
     actionSheetHidden: true,
-    actionSheetItems: ['item1', 'item2', 'item3']
+    actionSheetItems: ['item1', 'item2', 'item3'],
+    hidden: false,
+    nocancel: false
   },
   actionSheet: function () {
     this.setData({
@@ -20,11 +22,49 @@ Page({
   },
   onShow: function () {
     // 页面显示
+    wx.showToast({
+      title: '成功',
+      icon: 'success',
+      duration: 2000
+    })
+
+    wx.showLoading({
+      title: '加载中',
+    })
+
+    setTimeout(function () {
+      wx.hideLoading()
+    }, 2000)
+
+    wx.showModal({
+      title: '提示',
+      content: '这是一个模态弹窗',
+      success: function (res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
   }, 
   onHide: function () {
     // 页面隐藏
   },
   onUnload: function () {
     // 页面关闭
+  },
+
+
+  cancel: function () {
+    this.setData({
+      hidden: true
+    });
+  },
+  confirm: function () {
+    this.setData({
+      nocancel: !this.data.nocancel
+    });
+    console.log("clicked confirm");
   }
 })
