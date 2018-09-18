0e0f866f7b7aa390a36df9be42a458af446387a7
greatabel <myreceiver2for2github@gmail.com>
2018-09-18 11:08:51 +0800

    actionsheet

--- MINA/TemplateDemo/pages/interact/interact.js
+++ MINA/TemplateDemo/pages/interact/interact.js
@@ -1,66 +1,30 @@
-// pages/interact/interact.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    actionSheetHidden: true,
+    actionSheetItems: ['item1', 'item2', 'item3']
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  actionSheet: function () {
+    this.setData({
+      actionSheetHidden: !this.data.actionSheetHidden
+    });
+  },
+  //显示是否点击成功
+  itemTap: function () {
+    console.log('tap ' + e.currentTarget.dataset.name);
+  },
   onLoad: function (options) {
-
+    // 页面初始化 options为页面跳转所带来的参数
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
-
+    // 页面渲染完成
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
+    // 页面显示
+  }, 
   onHide: function () {
-
+    // 页面隐藏
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    // 页面关闭
   }
 })
