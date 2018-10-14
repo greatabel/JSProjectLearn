0fec9dbbc9170e13256ff4ea7e594e77e3a12d28
greatabel <myreceiver2for2github@gmail.com>
2018-10-14 23:54:38 +0800

    invoke data.js

--- MINA/i01Orange_can/pages/post/post.js
+++ MINA/i01Orange_can/pages/post/post.js
@@ -1,64 +1,66 @@
+var dataObj = require("../../data/data.js")
 // pages/post/post.js
 Page({
 
   /**
    * 页面的初始数据
    */
-  data: {
-    object: {
-      date: "Oct 05 2018"
-    },
+  data: {},
+//   data: {
+//     object: {
+//       date: "Oct 05 2018"
+//     },
     
-    title: "小时候的冰棍儿和雪糕",
-    postImg: "/images/post/post-4.jpg",
-    avatar: "/images/avatar/avatar-5.png",
-    content: "冰棍冰棍冰棍冰棍冰棍冰棍冰棍冰棍冰棍冰棍冰棍冰棍\
- 冰棍冰棍冰棍冰棍...",
-    readingNum: 92,
-    collectionNum: {
-      array: [104]
-    },
-    commentNum: 7
+//     title: "小时候的冰棍儿和雪糕",
+//     postImg: "/images/post/post-4.jpg",
+//     avatar: "/images/avatar/avatar-5.png",
+//     content: "冰棍冰棍冰棍冰棍冰棍冰棍冰棍冰棍冰棍冰棍冰棍冰棍\
+//  冰棍冰棍冰棍冰棍...",
+//     readingNum: 92,
+//     collectionNum: {
+//       array: [104]
+//     },
+//     commentNum: 7
 
-  },
+//   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var iceCreamData = {
-      object: {
-        date: 'Oct 06 2018'
-      },
-      title: "新的小时候冰棍儿",
-      postImg: "/images/post/post-4.jpg",
-      avatar: "/images/avatar/avatar-5.png",
-      content: "冰棍#冰棍冰棍冰棍冰棍冰棍冰棍冰棍冰棍冰棍冰棍冰棍\
- 冰棍冰棍冰棍冰棍#...",
-      readingNum: 92,
-      collectionNum: {
-        array: [103]
-      },
-      commentNum: 7
-    }
-    var trainData = {
-      object: {
-        date: 'Oct 07 2018'
-      },
-      title: "新的小时候火车",
-      postImg: "/images/post/post-1.jpg",
-      avatar: "/images/avatar/avatar-3.png",
-      content: "火车火车火车火车火车火车\
- 火车火车火车火车火车#...",
-      readingNum: 93,
-      collectionNum: {
-        array: [104]
-      },
-      commentNum: 9
-    }
-    var postList = [iceCreamData, trainData]
+//     var iceCreamData = {
+//       object: {
+//         date: 'Oct 06 2018'
+//       },
+//       title: "新的小时候冰棍儿",
+//       postImg: "/images/post/post-4.jpg",
+//       avatar: "/images/avatar/avatar-5.png",
+//       content: "冰棍#冰棍冰棍冰棍冰棍冰棍冰棍冰棍冰棍冰棍冰棍冰棍\
+//  冰棍冰棍冰棍冰棍#...",
+//       readingNum: 92,
+//       collectionNum: {
+//         array: [103]
+//       },
+//       commentNum: 7
+//     }
+//     var trainData = {
+//       object: {
+//         date: 'Oct 07 2018'
+//       },
+//       title: "新的小时候火车",
+//       postImg: "/images/post/post-1.jpg",
+//       avatar: "/images/avatar/avatar-3.png",
+//       content: "火车火车火车火车火车火车\
+//  火车火车火车火车火车#...",
+//       readingNum: 93,
+//       collectionNum: {
+//         array: [104]
+//       },
+//       commentNum: 9
+//     }
+//     var postList = [iceCreamData, trainData]
     this.setData({
-      postList: postList
+      postList: dataObj.postList
     })
   },
 
