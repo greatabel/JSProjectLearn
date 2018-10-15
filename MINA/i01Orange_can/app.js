83e4721e4a5c1135b6d097a4bf27514430b4ae36
greatabel <myreceiver2for2github@gmail.com>
2018-10-15 17:20:06 +0800

    use storage

--- MINA/i01Orange_can/app.js
+++ MINA/i01Orange_can/app.js
@@ -1,7 +1,20 @@
 //app.js
+var dataObj = require("data/data.js")
 App({
   onLaunch: function () {
+    wx.setStorage({
+      key: 'postList',
+      data: dataObj.postList,
+      success: function(res){
 
+      },
+      fail: function(){
+
+      },
+      complete: function(){
+        
+      }
+    })
   },
   globalData: {
 
