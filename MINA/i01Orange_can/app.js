45e29504ce64953bf6d9f2a7fbaa6ab0c7266b1f
greatabel <myreceiver2for2github@gmail.com>
2018-10-15 17:31:53 +0800

    storageData

--- MINA/i01Orange_can/app.js
+++ MINA/i01Orange_can/app.js
@@ -1,20 +1,27 @@
 //app.js
-var dataObj = require("data/data.js")
+
 App({
   onLaunch: function () {
-    wx.setStorage({
-      key: 'postList',
-      data: dataObj.postList,
-      success: function(res){
+    // wx.setStorage({
+    //   key: 'postList',
+    //   data: dataObj.postList,
+    //   success: function(res){
 
-      },
-      fail: function(){
+    //   },
+    //   fail: function(){
 
-      },
-      complete: function(){
-        
-      }
-    })
+    //   },
+    //   complete: function(){
+
+    //   }
+    // })
+    var storageData = wx.getStorageSync("postList");
+    if(!storageData){
+      var dataObj = require("data/data.js")
+      wx.clearStorageSync();
+      wx.setStorageSync("postList", dataObj.postList); 
+    }
+
   },
   globalData: {
 
