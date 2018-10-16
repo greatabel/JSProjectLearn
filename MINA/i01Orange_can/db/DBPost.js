d6063d836314baf6465e34fe967dca8270a462ec
greatabel <myreceiver2for2github@gmail.com>
2018-10-16 16:39:28 +0800

    DBPost

---
+++ MINA/i01Orange_can/db/DBPost.js
@@ -0,0 +1,16 @@
+var DBPost = function() {
+  this.storageKeyName = "postList";
+}
+
+DBPost.prototype = {
+  getAllPostData: function(){
+
+  },
+  execSetStorageSync: function(data){
+
+  },
+};
+
+module.exports = {
+  DBPost: DBPost
+};
