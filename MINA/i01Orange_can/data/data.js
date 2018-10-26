33772200b5e61e471e6d472cba8155d1e23ddd26
greatabel <myreceiver2for2github@gmail.com>
2018-10-26 16:39:19 +0800

    preView image

--- MINA/i01Orange_can/data/data.js
+++ MINA/i01Orange_can/data/data.js
@@ -49,7 +49,7 @@ var postList = [{
             create_time: '1484723344',
             content: {
                 txt: ' 那一年的毕业季，我们挥挥手，来不及说再见，就踏上了远行的火车。',
-                img: ["/images/comment/train-1.jpg","/images/comment/train-2.jpg","/images/comment/train-3.jpg"],
+              img: ["https://inews.gtimg.com/newsapp_bt/0/5968321973/1000","https://inews.gtimg.com/newsapp_bt/0/5990317371/1000","/images/comment/train-3.jpg"],
                 audio: null
             }
         }, {
--- MINA/i01Orange_can/pages/post/post-comment/post-comment.js
+++ MINA/i01Orange_can/pages/post/post-comment/post-comment.js
@@ -69,5 +69,21 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+
+  //预览图片
+  previewImg: function (event) {
+    //获取评论序号
+    var commentIdx = event.currentTarget.dataset.commentIdx,
+      //获取图片在图片数组中的序号
+      imgIdx = event.currentTarget.dataset.imgIdx,
+      //获取评论的全部图片
+      imgs = this.data.comments[commentIdx].content.img;
+    wx.previewImage({
+      current: imgs[imgIdx], // 当前显示图片的http链接
+      urls: imgs // 需要预览的图片http链接列表
+    })
+  },
+
+  
 })
