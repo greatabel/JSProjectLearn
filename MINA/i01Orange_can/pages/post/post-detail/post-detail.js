fd7dd98fd9170ae5362a1c6d931c38861ccc1489
greatabel <myreceiver2for2github@gmail.com>
2018-11-22 11:32:18 +0800

    switch musicStart/Stop icon

--- MINA/i01Orange_can/pages/post/post-detail/post-detail.js
+++ MINA/i01Orange_can/pages/post/post-detail/post-detail.js
@@ -6,7 +6,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    isPlayingMusic: false
   },
 
   /**
@@ -114,5 +114,10 @@ Page({
   addReadingTimes: function () {
     this.dbPost.addReadingTimes();
   },
+  onMusicTap: function(event) {
+    this.setData({
+      isPlayingMusic: !this.data.isPlayingMusic
+    })
+  }
 
 })
