990360fa30d5f6812112c57ed1876e84ce6cf089
greatabel <myreceiver2for2github@gmail.com>
2020-03-16 18:10:04 +0800

    mock_douban_api.py

---
+++ MINA/mock_douban_api/mock_douban_api.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, request
+from http import HTTPStatus
+
+app = Flask(__name__)
+
+in_theaters_movies = {
+"count": 20,
+"start": 0,
+"total": 38,
+"subjects": [{'id': 1325007, 'title': '蓝色星球 The Blue Planet', 'rating': 4, 
+'images': {'large': 'https://img3.doubanio.com/view/photo/l/public/p2574122772.webp'}, 
+'summary': '蓝色星球」是历年来首套全面探索海洋世界的自然历史专辑'}, 
+            {'id': 3041294, 'title': '生化危机4：战神再生', 'rating': 4.5, 
+'images': {'large': 'https://img9.doubanio.com/view/photo/l/public/p564897015.webp'}, 
+'summary': '爱丽丝（米拉·乔沃维奇 Milla Jovovich 饰）重回在东京，向安布雷拉公司复仇'}],
+"title": "正在上映的电影-深圳"
+}
+
+movies = {}
+
+@app.route("/mock_douban_api/in_theaters", methods=['GET'])
+def in_theaters():
+    return jsonify({'data': in_theaters_movies})
+
+@app.route("/mock_douban_api/comming_soon", methods=['GET'])
+def comming_soon():
+    return jsonify({'data': movies})
+
+@app.route("/mock_douban_api/top250", methods=['GET'])
+def top250():
+    return jsonify({'data': movies})
+
+
+
+
+# 使用httpie测试：http GET http://127.0.0.1:5000/recipes
+if __name__ == "__main__":
+    app.run()
