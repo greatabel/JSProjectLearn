0beb06124094a35c5741918127549456ae80a155
greatabel <myreceiver2for2github@gmail.com>
2020-03-16 21:34:33 +0800

    onMoreTap func

--- MINA/i01Orange_can/pages/movie/movie.js
+++ MINA/i01Orange_can/pages/movie/movie.js
@@ -88,4 +88,11 @@ Page({
     this.setData(readyData)
   },
 
+  onMoreTap: function (event) {
+    var category = event.currentTarget.dataset.category;
+    wx.navigateTo({
+      url: "more-movie/more-movie?category=" + category
+    })
+  }
+
 })
