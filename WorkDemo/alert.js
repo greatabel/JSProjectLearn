995f260a2c2612972b6a89eb3c55d6725cb23093
greatabel <myreceiver2for2github@gmail.com>
2020-10-12 14:35:39 +0800

    alert.js

--- WorkDemo/alert.js
+++ WorkDemo/alert.js
@@ -1,6 +1,6 @@
-var expression = /^https?:\/\//;
+var expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
 var regex = new RegExp(expression);
-var t = 'https://www.google.com';
+var t = 'www.google.com';
 
 if (t.match(regex)) {
   alert("Successful match");
